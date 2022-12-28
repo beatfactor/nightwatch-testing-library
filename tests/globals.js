b14ceac905e7ee042c6543bc233676ba0698629c
Andrei Rusu <beatfactor@users.noreply.github.com>
2022-12-28 17:38:07 +0100

    Update globals.js

--- tests/globals.js
+++ tests/globals.js
@@ -16,7 +16,7 @@ module.exports = {
     waitOn({
       resources: [`http://localhost:${serverPort}`]
     }).then(() => {
-      setTimeout(done, 1500);
+      setTimeout(done, 500);
     });
 
   },
